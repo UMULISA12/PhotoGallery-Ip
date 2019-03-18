555ee61c0c956009b8fa5726f3c107a472e67d61
UMULISA12 <umulisaa0@gmail.com>
2019-03-18 15:06:54 +0200

    adding style

--- photos/urls.py
+++ photos/urls.py
@@ -1,18 +1,3 @@
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# from django.conf.urls import url
-# from . import views
-
-# urlpatterns=[
-#     url('^$',views.welcome,name = 'welcome'),
-#     url('^today/$',views.one_photo,name='photosToday'),
-#     url(r'^search/', views.search_results, name='search_results')
-# ]
-
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
 from django.conf import settings
 from django.conf.urls.static import static
 
